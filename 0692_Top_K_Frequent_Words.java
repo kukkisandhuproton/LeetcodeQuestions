fc4693533132481d7d30b9e5fa7b6d913ad605cd
kukkisandhuproton <131423888+kukkisandhuproton@users.noreply.github.com>
2023-07-10 21:18:17 -0400

    Create 0692_Top_K_Frequent_Words.java

---
+++ 0692_Top_K_Frequent_Words.java
@@ -0,0 +1,26 @@
+5
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        List<String> result = new ArrayList<>();
+
+        HashMap<String,Integer> map = new HashMap<>();
+        for(String word:words){
+            map.put(word,map.getOrDefault(word,0)+1);
+        }
+        PriorityQueue<String> pq = new PriorityQueue<>((s1,s2)->{
+            if(map.get(s1)==map.get(s2)){
+                return s1.compareTo(s2);
+            }
+            else{
+                return map.get(s2)-map.get(s1);
+            }
+        });
+        pq.addAll(map.keySet());
+        while(k>0){
+            result.add(pq.poll());
+            k--;
+        }
+
+        return result;
+    }
+}
