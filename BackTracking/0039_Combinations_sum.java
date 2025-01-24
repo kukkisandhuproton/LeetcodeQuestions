b2e8117a9a70cdca29be7d671d8622f15d3f856b
kukkisandhuproton <131423888+kukkisandhuproton@users.noreply.github.com>
2025-01-24 11:57:01 -0500

    Update 0039_Combinations_sum.java

--- BackTracking/0039_Combinations_sum.java
+++ BackTracking/0039_Combinations_sum.java
@@ -22,3 +22,29 @@ class Solution {
 
     }
 }
+
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(candidates == null || candidates.length == 0){
+            return result;
+        }
+        backtrack(candidates, target, result, new ArrayList<>(), 0);
+        return result;
+    }
+
+    public void backtrack(int[]nums, int t, List<List<Integer>> result, List<Integer>current, int i){
+        if( t == 0){
+            result.add(new ArrayList<>(current));
+            return;
+        }
+        if(t<0 || i>=nums.length){
+            return;
+        }
+        for(int k = i; k<nums.length; k++){
+            current.add(nums[k]);
+            backtrack(nums,t-nums[k],result, current,k);
+            current.remove(current.size()-1);
+        }
+    }
+}
